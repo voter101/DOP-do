72433eb1c50dd986c4e2a306284c9c8b86ecd638
Wiktor Mociun <voter101@gmail.com>
2014-03-28 19:38:41 +0100

    Author management

--- app/helpers/services/tasks_service.rb
+++ app/helpers/services/tasks_service.rb
@@ -1,11 +1,18 @@
 class TasksService
   class TaskContentTooLongError < StandardError; end
 
+  attr_reader :user
+
+  def initialize(user)
+    @user = user
+  end
+
   def addTask(content)
     raise TaskContentTooLongError.new unless content.length > 3 && content.length < 255
 
     task = Task.new
     task.content = content
+    task.author = @user.id
     task.save
   end
 
