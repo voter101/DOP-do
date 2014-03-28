c9b05058f66d2d19af55aad29c42a9d8dbbd61d5
Wiktor Mociun <voter101@gmail.com>
2014-03-28 19:36:12 +0100

    Length controll

--- app/helpers/services/tasks_service.rb
+++ app/helpers/services/tasks_service.rb
@@ -2,7 +2,7 @@ class TasksService
   class TaskContentTooLongError < StandardError; end
 
   def addTask(content)
-    raise TaskContentTooLongError.new unless content.length > 3
+    raise TaskContentTooLongError.new unless content.length > 3 && content.length < 255
 
     task = Task.new
     task.content = content
