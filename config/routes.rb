e60a27670fa2ab47c04c19239c07679ca3977000
Wiktor Mociun <voter101@gmail.com>
2014-03-29 08:11:55 +0100

    Routes change for better logic

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ DOPDo::Application.routes.draw do
   root 'application#index'
 
   get '/tasks/getPicked', to: 'tasks#getPicked'
-  get '/tasks/pick', to: 'tasks#pick'
-  put '/tasks/done', to: 'tasks#markPickedAsDone'
+  get '/task/pick', to: 'tasks#pick'
+  put '/task/done', to: 'tasks#markPickedAsDone'
 
   get '/tasks', to: 'tasks#show'
-  get '/tasks/:id', to: 'tasks#showItem'
+  get '/task/:id', to: 'tasks#showItem'
   delete '/tasks/:id', to: 'tasks#destroy'
   post '/tasks', to: 'tasks#create'
 
