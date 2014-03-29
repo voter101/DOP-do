eb217c506db681bcf4402dd24fca1d0cf6683627
Wiktor Mociun <voter101@gmail.com>
2014-03-29 07:06:58 +0100

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 DOPDo::Application.routes.draw do
   devise_for :users
+
   root 'application#index'
+
   get '/tasks', to: 'tasks#show'
   get '/tasks/:id', to: 'tasks#showItem'
   delete '/tasks/:id', to: 'tasks#destroy'
   post '/tasks', to: 'tasks#create'
+
+
+  get '/tasks/getPicked', to: 'tasks#getPicked'
+  get '/tasks/pick', to: 'tasks#pick'
 end
