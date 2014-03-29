4ad3feb7d0a6a81574207c513d3e0d96a517a743
Wiktor Mociun <voter101@gmail.com>
2014-03-29 08:12:23 +0100

    Reorder

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ DOPDo::Application.routes.draw do
 
   root 'application#index'
 
-  get '/tasks/getPicked', to: 'tasks#getPicked'
   get '/task/pick', to: 'tasks#pick'
   put '/task/done', to: 'tasks#markPickedAsDone'
-
-  get '/tasks', to: 'tasks#show'
   get '/task/:id', to: 'tasks#showItem'
+
+  get '/tasks/getPicked', to: 'tasks#getPicked'
+  get '/tasks', to: 'tasks#show'
   delete '/tasks/:id', to: 'tasks#destroy'
   post '/tasks', to: 'tasks#create'
 
