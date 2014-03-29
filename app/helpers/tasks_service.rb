a35eb63faf2f37406474809c0e30cb8adea9aca6
Wiktor Mociun <voter101@gmail.com>
2014-03-29 03:29:52 +0100

    I got to move this here + Refactoring for easier development

--- app/helpers/tasks_service.rb
+++ app/helpers/tasks_service.rb
@@ -1,19 +1,19 @@
 class TasksService
   class TaskContentTooLongError < StandardError; end
 
-  attr_reader :user
+  #attr_reader :user
 
-  def initialize(user)
-    @user = user
-  end
+  #def initialize(user)
+  #  @user = user
+  #end
 
   def addTask(content)
     raise TaskContentTooLongError.new unless content.length > 3 && content.length < 255
 
     task = Task.new
     task.content = content
-    task.author = @user.id
+    #task.author = @user.id
+    task.author = "123"
     task.save
   end
-
 end
