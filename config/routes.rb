5f3e3b64205a260d76baea6572fa6d523f19aff5
Wiktor Mociun <voter101@gmail.com>
2014-03-29 04:22:40 +0100

    New route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ DOPDo::Application.routes.draw do
   root 'application#index'
   get '/tasks', to: 'tasks#show'
   get '/tasks/:id', to: 'tasks#showItem'
+  delete '/tasks/:id', to: 'tasks#destroy'
   post '/tasks', to: 'tasks#create'
 end
