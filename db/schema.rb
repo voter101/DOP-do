24bb61881bdeb586d696fbdd1b31204f11222e75
Wiktor Mociun <voter101@gmail.com>
2014-03-29 06:39:52 +0100

    Schema change

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20140329044736) do
   create_table "tasks", force: true do |t|
     t.string "content",              null: false
     t.string "author",   limit: 128, null: false
-    t.string "taken_by", limit: 128
+    t.string "picked_by", limit: 128
   end
 
   create_table "users", force: true do |t|
